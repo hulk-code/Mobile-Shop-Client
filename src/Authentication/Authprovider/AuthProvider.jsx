abae99a91b0cd84c09fea6375143cb171cd2e081
Akash <shoaibsarkarakash019@gmail.com>
2023-12-23 20:13:02 +0600

    create new user

--- src/Authentication/Authprovider/AuthProvider.jsx
+++ src/Authentication/Authprovider/AuthProvider.jsx
@@ -20,7 +20,13 @@ const googleProvider = new GoogleAuthProvider();
 
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
- 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+  
+    const createUser = (email, password) => {
+      setLoading(true);
+      return createUserWithEmailAndPassword(auth, email, password);
+    };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
